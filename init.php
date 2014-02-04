656c9ad1899f231fa802f0d0c38d284ce215e1ed
Michaël Marinetti <github@marinetti.fr>
2014-02-04 10:45:39 +0100

    Mod: vendor file included only if class not already exists

--- init.php
+++ init.php
@@ -1,22 +1,25 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-// Find the FirePHP vendor class
-$vendor_path = Kohana::find_file('vendor/firephp-core/lib/FirePHPCore','FirePHP.class');
-
-if ($vendor_path === FALSE)
+if (!class_exists('FirePHP', false))
 {
-	/**
-	 * If the vendor doesn't exist, user probably didn't update submodules so
-	 * warn him instead of ending up with a fatal error. Updating submodules:
-	 *
-	 * 		git submodule update --init
-	 *
-	 * (inside of the firephp submodule folder)
-	 */
-	throw new Kohana_Exception('You must update submodules (vendor class) to make FirePHP work with Kohana.');
-}
+  // Find the FirePHP vendor class
+  $vendor_path = Kohana::find_file('vendor/firephp-core/lib/FirePHPCore','FirePHP.class');
 
-require_once $vendor_path;
+  if ($vendor_path === FALSE)
+  {
+    /**
+     * If the vendor doesn't exist, user probably didn't update submodules so
+     * warn him instead of ending up with a fatal error. Updating submodules:
+     *
+     * 		git submodule update --init
+     *
+     * (inside of the firephp submodule folder)
+     */
+    throw new Kohana_Exception('You must update submodules (vendor class) to make FirePHP work with Kohana.');
+  }
+
+  require_once $vendor_path;
+}
 
 $fire_logger = new Fire_Log;
 
@@ -26,5 +29,6 @@ Kohana::$log->attach($fire_logger);
 // Automatically disable FirePHP logging in production phase
 if (Kohana::$environment === Kohana::PRODUCTION)
 {
-	$fire_logger->writer()->setEnabled(FALSE);
+  $fire_logger->writer()->setEnabled(FALSE);
 }
+
